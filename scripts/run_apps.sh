25ff94579cc2338313a41bfff9440fa4f5bf0311
tre island <t.island84@gmail.com>
2023-10-18 18:06:22 -0400

    moved apps to subdirectory

--- scripts/run_apps.sh
+++ scripts/run_apps.sh
@@ -4,6 +4,6 @@ BASEDIR=$(dirname $0)
 MK8S_SAMPLE_NAMESPACE=sample-apps
 
 microk8s kubectl create namespace $MK8S_SAMPLE_NAMESPACE
-microk8s kubectl -n $MK8S_SAMPLE_NAMESPACE apply -f $BASEDIR/../juice-shop.yaml
-microk8s kubectl -n $MK8S_SAMPLE_NAMESPACE apply -f $BASEDIR/../webgoat.yaml
+microk8s kubectl -n $MK8S_SAMPLE_NAMESPACE apply -f $BASEDIR/../apps/juice-shop.yaml
+microk8s kubectl -n $MK8S_SAMPLE_NAMESPACE apply -f $BASEDIR/../apps/webgoat.yaml
 
